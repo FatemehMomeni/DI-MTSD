82a1a354be715b961b8410ab3fce573731cf0d59
FatemehMomeni <50900356+FatemehMomeni@users.noreply.github.com>
2023-05-05 12:18:27 +0330

    Update preprocessing.py

--- src/utils/preprocessing.py
+++ src/utils/preprocessing.py
@@ -4,6 +4,64 @@ import wordninja
 import csv
 import pandas as pd
 
+import statistics
+import pandas as pd
+import numpy as np
+import string
+from sentence_transformers import SentenceTransformer, util
+from sklearn.feature_extraction.text import CountVectorizer
+
+def related_targets(clean_data, x_target):
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+
+    targets = list()
+    related_terms = dict()
+    for tar in x_target:      
+      tar_phrase = ''      
+      for tar_word in tar:        
+        tar_phrase = tar_phrase + tar_word + ' '        
+      tar_phrase = tar_phrase.rstrip()      
+      if tar_phrase not in targets:
+        targets.append(tar_phrase)
+        related_terms[tar_phrase] = list()
+    
+    punct = set(string.punctuation)
+    tokens = list()
+    for msg in clean_data:      
+      for word in msg:        
+        if word not in punct:
+          tokens.append(word)
+
+    msg_embeddings = model.encode(tokens)
+    for tar in targets:
+        target_embedding = model.encode(tar)
+        cos_sim = util.cos_sim(target_embedding, msg_embeddings)
+
+        similars = list()
+        for arr in cos_sim:
+            for i, score in enumerate(arr):
+                if score >= 0.7:
+                    similars.append([tokens[i], score])
+        similars = [x for x in similars if similars.count(x) == 1]
+        sorted_similars = sorted(similars, key=lambda x: x[1], reverse=True)        
+
+        for token_score in sorted_similars:
+            related_terms[tar].append([token_score[0]])  
+
+    for key,value in related_terms.items():
+        if len(value) == 3:
+            related_terms[key] = value[:3]
+        else:
+            while len(value) < 3:
+                related_terms[key].append([k for k in key.split()])
+        if type(key) == str:
+          new_key = key.split()
+          new_key = tuple(new_key)
+          related_terms[new_key] = related_terms.pop(key)
+        else:
+          break
+        
+    return related_terms
 
 # Data Loading
 def load_data(filename,usecols,col,dataset_name):
@@ -59,4 +117,6 @@ def clean_all(filename,col,dataset_name,norm_dict):
         clean_data[i] = data_clean(raw_data[i],norm_dict) # clean each tweet text [['word1','word2'],[...],...]
         x_target[i] = data_clean(x_target[i],norm_dict)
     
-    return clean_data,label,x_target
+    x_target_set = related_targets(clean_data, x_target)
+    
+    return clean_data,label,x_target,x_target_set
