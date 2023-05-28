014580dfdad2db05ab5c1cde3a541cdee0d77fb8
FatemehMomeni <50900356+FatemehMomeni@users.noreply.github.com>
2023-05-28 17:52:07 +0330

    Update train_model.py

--- src/batch/train_model.py
+++ src/batch/train_model.py
@@ -8,9 +8,9 @@ import argparse
 import json
 import gc
 import gspread
-import utils.preprocessing as pp
-import utils.data_helper as dh
 from transformers import AdamW
+import preprocessing as pp
+import data_helper as dh
 import sys
 sys.path.append('/content/DI-MTSD/src')
 from utils import modeling, model_eval
